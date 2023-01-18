1c5c58e7842540d6ca1442e621b052f7fa18955b
Adel Rodríguez <hello@adelrodriguez.com>
2023-01-17 23:31:38 -0400

    add variants to button

--- app/components/common/Button.tsx
+++ app/components/common/Button.tsx
@@ -7,23 +7,31 @@ export default function Button({
   className,
   size = "md",
   disabled,
+  variant = "primary",
   ...props
 }: {
   children: ReactNode
   className?: string
   size?: "xs" | "sm" | "md" | "lg" | "xl"
+  variant?: "primary" | "secondary"
 } & ButtonHTMLAttributes<HTMLButtonElement>): ReactElement {
   return (
     <button
       type={type}
       className={clsx(
-        "inline-flex items-center justify-center border border-transparent bg-gray-600 font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2",
+        "inline-flex items-center justify-center border   font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2",
         {
-          "rounded px-2.5 py-1.5 text-xs": size === "xs",
-          "rounded-md px-3 py-2 text-sm leading-4": size === "sm",
+          "rounded px-2.5 py-2 text-xs": size === "xs",
+          "rounded-md px-3 py-2 text-sm": size === "sm",
           "rounded-md px-4 py-2 text-base": size === "lg",
           "rounded-md px-4 py-2 text-sm": size === "md",
           "rounded-md px-6 py-3 text-base": size === "xl",
+        },
+        {
+          "border-gray-300 bg-white text-gray-700 hover:bg-gray-50 focus:ring-gray-500":
+            variant === "secondary",
+          "border-transparent bg-gray-600 text-white hover:bg-gray-700 focus:ring-gray-500":
+            variant === "primary",
         },
         { "opacity-50 cursor-not-allowed": disabled },
         className
