25f364fd70f2d1ec7609b32d299da2885e4ddb45
Adel Rodríguez <hello@adelrodriguez.com>
2023-01-17 23:33:45 -0400

    create CartNote component

---
+++ app/components/registry/CartNote.tsx
@@ -0,0 +1,91 @@
+import { Dialog, Transition } from "@headlessui/react"
+import { EnvelopeIcon } from "@heroicons/react/24/solid"
+import { Fragment } from "react"
+
+import { Button, TextArea } from "~/components/common"
+import { useCart } from "~/utils/hooks"
+
+export default function CartNote({
+  open,
+  onClose,
+}: {
+  open: boolean
+  onClose: () => void
+}) {
+  const cart = useCart()
+
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={onClose}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                <div className="sm:flex sm:items-start">
+                  <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-gray-100 sm:mx-0 sm:h-10 sm:w-10">
+                    <EnvelopeIcon
+                      className="h-6 w-6 text-gray-600"
+                      aria-hidden="true"
+                    />
+                  </div>
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-full">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-lg font-medium leading-6 text-gray-900"
+                    >
+                      Add a message
+                    </Dialog.Title>
+                    <div className="mt-2 w-full">
+                      <TextArea
+                        className="w-full"
+                        value={cart.note}
+                        onChange={(e) => cart.setNote(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                  <Button
+                    size="sm"
+                    className="w-full sm:w-auto sm:ml-3"
+                    onClick={() => onClose()}
+                  >
+                    Save
+                  </Button>
+                  <Button
+                    type="button"
+                    className="mt-3 inline-flex w-full justify-center  sm:mt-0 sm:w-auto sm:text-sm"
+                    onClick={() => onClose()}
+                    variant="secondary"
+                  >
+                    Cancel
+                  </Button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  )
+}
--- app/components/registry/index.ts
+++ app/components/registry/index.ts
@@ -1,3 +1,4 @@
+export { default as CartItem } from "./CartItem"
+export { default as CartNote } from "./CartNote"
 export { default as ListingItem } from "./ListingItem"
 export { default as QuantityInput } from "./QuantityInput"
-export { default as CartItem } from "./CartItem"
