e5eae231bad6edaa1b555c7e18919f2bbe844e1e
Adel Rodríguez <hello@adelrodriguez.com>
2023-01-18 19:21:50 -0400

    add loading state to the ListingItem

--- app/components/registry/ListingItem.tsx
+++ app/components/registry/ListingItem.tsx
@@ -1,8 +1,9 @@
 import type { Item } from "@prisma/client"
 import { Link } from "@remix-run/react"
+import * as Sentry from "@sentry/remix"
+import type { ReactElement } from "react"
 
-import { FormattedNumber } from "~/components/common"
-import { Spinner } from "~/components/loading"
+import { FormattedNumber, Image } from "~/components/common"
 import { useProduct } from "~/utils/hooks"
 import { getPriceSymbol } from "~/utils/money"
 
@@ -14,33 +15,44 @@ export default function ListingItem({
   id: Item["id"]
   title: Item["title"]
   commerceId: string
-}) {
-  const { data, isLoading, isError } = useProduct(commerceId)
+}): ReactElement | null {
+  const { data, isLoading, isError, error } = useProduct(commerceId)
 
-  // TODO(adelrodriguez): Handle loading and error states
   if (isLoading) {
     return (
-      <div className="h-48 w-full aspect-w-1">
-        <Spinner className="text-gray" />
+      <div className="animate-pulse">
+        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden sm:rounded-lg xl:aspect-w-7 xl:aspect-h-8">
+          <div className="h-full w-full bg-gray-200" />
+        </div>
+        <div className="mt-4 space-y-4">
+          <div className="h-4 bg-gray-200 rounded" />
+          <div className="space-y-2">
+            <div className="h-4 bg-gray-200 rounded w-1/4" />
+          </div>
+        </div>
       </div>
     )
   }
 
-  if (isError) return <div>Error!</div>
+  if (isError) {
+    Sentry.captureException(error)
 
-  const price = data.product?.variants.nodes[0]?.price!
+    return null
+  }
+
+  const price = data?.product?.variants.nodes[0]?.price!
 
   return (
     <Link className="group" to={id}>
-      <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden sm:rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-        <img
+      <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden sm:rounded-lg xl:aspect-w-7 xl:aspect-h-8">
+        <Image
           src={data?.product?.variants.nodes[0]?.image?.url}
-          alt={data?.product?.title}
+          alt={title}
           className="h-full w-full object-cover object-center group-hover:opacity-75"
         />
       </div>
       <h3 className="mt-4 text-sm text-gray-700">{title}</h3>
-      <p className="mt-1 text-lg font-medium text-gray-900">
+      <p className="mt-1 text-md font-medium text-gray-900">
         <FormattedNumber
           prefix={getPriceSymbol(price.currencyCode)}
           thousands
