86510f1c5cbf62ab9a1bbf76c7f407a0392e36f1
Adel Rodríguez <hello@adelrodriguez.com>
2023-01-27 23:17:31 -0400

    clean up Alert

--- app/components/common/Alert.tsx
+++ app/components/common/Alert.tsx
@@ -3,9 +3,9 @@ import {
   XMarkIcon,
   CheckCircleIcon,
 } from "@heroicons/react/20/solid"
+import clsx from "clsx"
 import type { ReactNode } from "react"
 
-// TODO(adelrodriguez): Clean up this component
 export default function Alert({
   children,
   onClose,
@@ -15,62 +15,56 @@ export default function Alert({
   type: "error" | "success"
   onClose: () => void
 }) {
-  if (type === "success") {
-    return (
-      <div className="rounded-md bg-green-50 p-4">
-        <div className="flex">
-          <div className="flex-shrink-0">
+  return (
+    <div
+      className={clsx("rounded-md p-4", {
+        "bg-green-50": type === "success",
+        "bg-red-50": type === "error",
+      })}
+    >
+      <div className="flex">
+        <div className="flex-shrink-0">
+          {type === "error" && (
+            <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
+          )}
+          {type === "success" && (
             <CheckCircleIcon
               className="h-5 w-5 text-green-400"
               aria-hidden="true"
             />
+          )}
+        </div>
+        <div className="ml-3">
+          <div
+            className={clsx("text-sm font-medium", {
+              "text-green-800": type === "success",
+              "text-red-800": type === "error",
+            })}
+          >
+            {children}
           </div>
-          <div className="ml-3">
-            <div className="text-sm font-medium text-green-800">{children}</div>
-          </div>
-          <div className="ml-auto pl-3">
-            <div className="-mx-1.5 -my-1.5">
-              <button
-                type="button"
-                className="inline-flex rounded-md bg-green-50 p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50"
-                onClick={onClose}
-              >
-                <span className="sr-only">Dismiss</span>
-                <XMarkIcon className="h-5 w-5" aria-hidden="true" />
-              </button>
-            </div>
+        </div>
+        <div className="ml-auto pl-3">
+          <div className="-mx-1.5 -my-1.5">
+            <button
+              type="button"
+              className={clsx(
+                "inline-flex rounded-md p-1.5  focus:outline-none focus:ring-2  focus:ring-offset-2",
+                {
+                  "bg-green-50 text-green-500 hover:bg-green-100 focus:ring-green-600 focus:ring-offset-green-50":
+                    type === "success",
+                  "bg-red-50 text-red-500 hover:bg-red-100 focus:ring-red-600 focus:ring-offset-red-50":
+                    type === "error",
+                }
+              )}
+              onClick={onClose}
+            >
+              <span className="sr-only">Dismiss</span>
+              <XMarkIcon className="h-5 w-5" aria-hidden="true" />
+            </button>
           </div>
         </div>
       </div>
-    )
-  }
-
-  if (type === "error") {
-    return (
-      <div className="rounded-md bg-red-50 p-4">
-        <div className="flex">
-          <div className="flex-shrink-0">
-            <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
-          </div>
-          <div className="ml-3">
-            <div className="text-sm font-medium text-red-800">{children}</div>
-          </div>
-          <div className="ml-auto pl-3">
-            <div className="-mx-1.5 -my-1.5">
-              <button
-                type="button"
-                className="inline-flex rounded-md bg-red-50 p-1.5 text-red-500 hover:bg-red-100 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2 focus:ring-offset-red-50"
-                onClick={onClose}
-              >
-                <span className="sr-only">Dismiss</span>
-                <XMarkIcon className="h-5 w-5" aria-hidden="true" />
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
-
-  throw new Error("Invalid alert type")
+    </div>
+  )
 }
