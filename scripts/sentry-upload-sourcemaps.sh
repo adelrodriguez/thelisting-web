147673fb45dd76d57cdcc15aaae9d16d8bcdd807
Adel Rodríguez <hello@adelrodriguez.com>
2023-02-21 13:27:05 -0400

    improve build sourcemaps script

--- scripts/sentry-upload-sourcemaps.sh
+++ scripts/sentry-upload-sourcemaps.sh
@@ -4,7 +4,7 @@ export SENTRY_PROJECT=app
 
 echo "Uploading sourcemaps to Sentry"
 
-node ./node_modules/@sentry/remix/scripts/sentry-upload-sourcemaps.js
+node ./node_modules/@sentry/remix/scripts/sentry-upload-sourcemaps.js --release $RAILWAY_GIT_COMMIT_SHA
 
 echo "Removing sourcemaps"
 
