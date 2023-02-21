bc831a51fb76e3ca8ba2dae31fe48bddc9d78f66
Adel Rodríguez <hello@adelrodriguez.com>
2023-02-21 13:12:29 -0400

    add uploading sourcemaps to build script

---
+++ scripts/sentry-upload-sourcemaps.sh
@@ -0,0 +1,11 @@
+export SENTRY_AUTH_TOKEN=$SENTRY_AUTH_TOKEN
+export SENTRY_ORG=thelisting
+export SENTRY_PROJECT=app
+
+echo "Uploading sourcemaps to Sentry"
+
+node ./node_modules/@sentry/remix/scripts/sentry-upload-sourcemaps.js
+
+echo "Removing sourcemaps"
+
+rm -v ./public/**/*.map
