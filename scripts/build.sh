9d9830d9054216839fce9dee70a50ff42de8ca63
Adel Rodríguez <hello@adelrodriguez.com>
2023-02-21 21:09:29 -0400

    test new build script

---
+++ scripts/build.sh
@@ -0,0 +1,28 @@
+export SENTRY_AUTH_TOKEN=$SENTRY_AUTH_TOKEN
+export SENTRY_ORG=thelisting
+export SENTRY_PROJECT=app
+
+VERSION=$RAILWAY_GIT_COMMIT_SHA
+
+npx remix build --sourcemap
+
+echo "Uploading sourcemaps to Sentry"
+
+sentry-cli releases new "$VERSION"
+sentry-cli releases set-commits --auto "$VERSION"
+
+echo "Uploading server sourcemaps"
+
+sentry-cli releases files "$VERSION" upload-sourcemaps ./build --url-prefix "~/build" --ignore node_modules --ignore public
+
+echo "Uploading client sourcemaps"
+
+sentry-cli releases files "$VERSION" upload-sourcemaps ./public --url-prefix "~/" --dist "client"
+
+sentry-cli releases finalize "$VERSION"
+
+echo "Removing sourcemaps"
+
+rm -v ./public/**/*.map
+rm -v ./build/*.map
+rm -v ./public/build/**/*.map
