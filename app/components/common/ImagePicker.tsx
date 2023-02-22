d489e02f7b86def74e25fc091b9cfd663b8d1609
Adel Rodríguez <hello@adelrodriguez.com>
2023-02-21 20:10:13 -0400

    improve imagepicker

--- app/components/common/ImagePicker.tsx
+++ app/components/common/ImagePicker.tsx
@@ -1,6 +1,5 @@
 import { Dialog, Transition } from "@headlessui/react"
-import { PhotoIcon as PhotoIconOutline } from "@heroicons/react/24/outline"
-import { PhotoIcon as PhotoIconSolid } from "@heroicons/react/24/solid"
+import { PhotoIcon } from "@heroicons/react/24/outline"
 import type { Image as UserImage } from "@prisma/client"
 import { useFetcher } from "@remix-run/react"
 import { useQuery } from "@tanstack/react-query"
@@ -27,17 +26,12 @@ export default function ImagePicker({
     ["images"],
     () => fetch("/api/user/images").then((res) => res.json())
   )
-  const [step, setStep] = useState<"choose" | "upload" | "name">("choose")
+  const [step, setStep] = useState<"choose" | "upload">("choose")
   const [file, setFile] = useState<File | null>(null)
 
   function handleUpload(uploadedFile: File) {
     setFile(uploadedFile)
-    setStep("name")
-  }
-
-  function handleUploadCancel() {
-    setFile(null)
-    setStep("choose")
+    setStep("upload")
   }
 
   function handleSubmitCancel() {
@@ -82,17 +76,11 @@ export default function ImagePicker({
                 {data && step === "choose" && (
                   <ImageGallery
                     images={data}
-                    onUploadClick={() => setStep("upload")}
+                    onUpload={handleUpload}
                     onSelect={onSelect}
                   />
                 )}
-                {step === "upload" && (
-                  <ImageUploader
-                    onUpload={handleUpload}
-                    onCancel={handleUploadCancel}
-                  />
-                )}
-                {step === "name" && file && (
+                {step === "upload" && file && (
                   <ImageSubmit
                     file={file}
                     onCancel={handleSubmitCancel}
@@ -110,14 +98,27 @@ export default function ImagePicker({
 
 function ImageGallery({
   images,
-  onUploadClick,
+  onUpload,
   onSelect,
 }: {
   images: UserImage[]
-  onUploadClick: () => void
   onSelect: (image: UserImage) => void
+  onUpload: (file: File) => void
 }) {
   const [selected, setSelected] = useState<UserImage | null>(null)
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    accept: {
+      "image/gif": [".gif"],
+      "image/jpg": [".jpg", ".jpeg"],
+      "image/png": [".png"],
+    },
+    maxFiles: 1,
+    onDrop: (files) => {
+      invariant(files[0], "No file was uploaded")
+
+      onUpload(files[0])
+    },
+  })
 
   return (
     <div className="h-full">
@@ -128,6 +129,45 @@ function ImageGallery({
       <div className="flex h-full flex-col gap-y-4 py-6 px-3">
         <div className="flex-1 overflow-auto p-4">
           <ul className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
+            <li className="h-full">
+              <button
+                {...getRootProps({
+                  className: clsx(
+                    "flex-1 relative block w-full rounded-lg p-6 md:py-11 text-center transition-all hover:border-gray-400  focus:shadow-lg focus:outline-none",
+                    {
+                      "bg-gray-100 shadow-lg": isDragActive,
+                      "border-2 border-dashed border-gray-300": !isDragActive,
+                    }
+                  ),
+                  type: "button",
+                })}
+              >
+                <div className="flex flex-col space-y-1 text-sm text-gray-600">
+                  <PhotoIcon className="mx-auto h-12 w-12 text-gray-400" />
+                  <div className="mx-auto flex flex-col lg:flex-row">
+                    <label
+                      htmlFor="file-upload"
+                      className="relative cursor-pointer rounded-md  font-medium text-gray-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-gray-500 focus-within:ring-offset-2 hover:text-gray-500"
+                    >
+                      Upload a file
+                    </label>
+                    <input
+                      {...getInputProps({
+                        className: "sr-only",
+                        id: "image-upload",
+                        name: "image-upload",
+                        type: "file",
+                      })}
+                    />
+                    <p className="pl-1">or drag and drop</p>
+                  </div>
+
+                  <p className="text-xs text-gray-500">
+                    JPG, PNG and GIF files up to 10MB
+                  </p>
+                </div>
+              </button>
+            </li>
             {images.map((image) => (
               <li key={image.id} className="relative">
                 <div className="group aspect-w-10 aspect-h-7 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
@@ -154,10 +194,6 @@ function ImageGallery({
           </ul>
         </div>
         <div className="flex justify-end gap-x-2">
-          <Button onClick={onUploadClick} variant="secondary">
-            <PhotoIconSolid className="mr-2 h-5 w-5" />
-            Upload
-          </Button>
           <Button
             onClick={() => {
               invariant(selected, "No image was selected")
@@ -166,83 +202,6 @@ function ImageGallery({
             disabled={!selected}
           >
             Select
-          </Button>
-        </div>
-      </div>
-    </div>
-  )
-}
-
-function ImageUploader({
-  onUpload,
-  onCancel,
-}: {
-  onUpload: (file: File) => void
-  onCancel: () => void
-}) {
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({
-    accept: {
-      "image/gif": [".gif"],
-      "image/jpg": [".jpg", ".jpeg"],
-      "image/png": [".png"],
-    },
-    maxFiles: 1,
-    onDrop: (files) => {
-      invariant(files[0], "No file was uploaded")
-
-      onUpload(files[0])
-    },
-  })
-
-  return (
-    <div className="h-full">
-      <div className="flex justify-between">
-        <Dialog.Title as="h3" className="text-xl">
-          Upload an image
-        </Dialog.Title>
-      </div>
-
-      <div className="mx-auto flex h-full w-full max-w-7xl flex-col gap-y-4 py-6 px-3">
-        <button
-          {...getRootProps({
-            className: clsx(
-              "flex-1 relative block w-full rounded-lg p-6 text-center transition-all hover:border-gray-400  focus:shadow-lg focus:outline-none",
-              {
-                "bg-gray-100 shadow-lg": isDragActive,
-                "border-2 border-dashed border-gray-300": !isDragActive,
-              }
-            ),
-            type: "button",
-          })}
-        >
-          <div className="flex flex-col space-y-1 text-sm text-gray-600">
-            <PhotoIconOutline className="mx-auto h-12 w-12 text-gray-400" />
-            <div className="mx-auto flex">
-              <label
-                htmlFor="file-upload"
-                className="relative cursor-pointer rounded-md  font-medium text-gray-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-gray-500 focus-within:ring-offset-2 hover:text-gray-500"
-              >
-                Upload a file
-              </label>
-              <input
-                {...getInputProps({
-                  className: "sr-only",
-                  id: "image-upload",
-                  name: "image-upload",
-                  type: "file",
-                })}
-              />
-              <p className="pl-1">or drag and drop</p>
-            </div>
-
-            <p className="text-xs text-gray-500">
-              JPG, PNG and GIF files up to 10MB
-            </p>
-          </div>
-        </button>
-        <div className="w-full">
-          <Button variant="secondary" onClick={onCancel}>
-            Cancel
           </Button>
         </div>
       </div>
@@ -268,6 +227,7 @@ function ImageSubmit({
   }, [fetcher.data, onSubmit])
 
   function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.stopPropagation()
     e.preventDefault()
     const data = new FormData(e.currentTarget)
     data.append("file", file)
