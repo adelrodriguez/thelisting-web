cf828c745a14c0acc0ab1654dc9cd107101ad7ea
Adel Rodríguez <hello@adelrodriguez.com>
2023-04-08 12:48:22 -0400

    temp disable webhook log

--- app/routes/api.webhooks.shopify.checkout-updated.ts
+++ app/routes/api.webhooks.shopify.checkout-updated.ts
@@ -1,4 +1,5 @@
 import type { ActionArgs } from "@remix-run/node"
+import { json } from "@remix-run/node"
 import { badRequest, serverError, unauthorized } from "remix-utils"
 import { z } from "zod"
 
@@ -11,63 +12,70 @@ import {
 } from "~/utils/shopify"
 import { checkWebhookLog, verifyWebhook } from "~/utils/webhook.server"
 
-export function loader() {
-  throw NotAllowed
-}
+// export function loader() {
+//   throw NotAllowed
+// }
 
+// TODO(adelrodriguez): TEMPORARY DISABLED
 export async function action({ request, context }: ActionArgs) {
-  const { logger, db } = context
+  const { logger } = context
 
-  const clone = request.clone()
-  const [json, text] = await Promise.all([clone.json(), request.text()])
-  const headers = request.headers
+  // TODO(adelrodriguez): remove this
+  logger.info({
+    headers: request.headers,
+    message: "Received webhook",
+  })
 
-  const isVerified = verifyWebhook(headers, text)
+  // const clone = request.clone()
+  // const [json, text] = await Promise.all([clone.json(), request.text()])
+  // const headers = request.headers
 
-  if (!isVerified) {
-    logger.error("Webhook not verified", { headers })
+  // const isVerified = verifyWebhook(headers, text)
 
-    throw unauthorized({ message: "Webhook not verified" })
-  }
+  // if (!isVerified) {
+  //   logger.error("Webhook not verified", { headers })
 
-  const { webhookId, event } = getShopifyWebhookHeaders(headers)
-  logger.info(`Received ${event} webhook`, { webhookId })
+  //   throw unauthorized({ message: "Webhook not verified" })
+  // }
 
-  const received = await checkWebhookLog(db, webhookId, event, "Shopify", json)
+  // const { webhookId, event } = getShopifyWebhookHeaders(headers)
+  // logger.info(`Received ${event} webhook`, { webhookId })
 
-  if (received) {
-    logger.info("Webhook already received. Ignoring...", { webhookId })
+  // const received = await checkWebhookLog(db, webhookId, event, "Shopify", json)
 
-    return Accepted
-  }
+  // if (received) {
+  //   logger.info("Webhook already received. Ignoring...", { webhookId })
 
-  try {
-    const checkout = parseCheckoutUpdateWebhookPayload(json)
+  //   return Accepted
+  // }
 
-    await db.checkout.upsert({
-      create: {
-        commerceId: getShopifyId(checkout.id, "Checkout"),
-        email: checkout.email,
-        name: checkout.billing_address?.name,
-        phone: checkout.billing_address?.phone,
-      },
-      update: {
-        completedAt: checkout.completed_at,
-        email: checkout.email,
-        name: checkout.billing_address?.name,
-        phone: checkout.billing_address?.phone,
-      },
-      where: { commerceId: getShopifyId(checkout.id, "Checkout") },
-    })
+  // try {
+  //   const checkout = parseCheckoutUpdateWebhookPayload(json)
 
-    return OK
-  } catch (error) {
-    Sentry.captureException(error)
+  //   await db.checkout.upsert({
+  //     create: {
+  //       commerceId: getShopifyId(checkout.id, "Checkout"),
+  //       email: checkout.email,
+  //       name: checkout.billing_address?.name,
+  //       phone: checkout.billing_address?.phone,
+  //     },
+  //     update: {
+  //       completedAt: checkout.completed_at,
+  //       email: checkout.email,
+  //       name: checkout.billing_address?.name,
+  //       phone: checkout.billing_address?.phone,
+  //     },
+  //     where: { commerceId: getShopifyId(checkout.id, "Checkout") },
+  //   })
 
-    if (error instanceof z.ZodError) {
-      return badRequest(error.message)
-    }
+  return json({ message: "OK" }, { status: 200 })
+  // } catch (error) {
+  //   Sentry.captureException(error)
 
-    return serverError({ message: (error as Error).message })
-  }
+  //   if (error instanceof z.ZodError) {
+  //     return badRequest(error.message)
+  //   }
+
+  //   return serverError({ message: (error as Error).message })
+  // }
 }
