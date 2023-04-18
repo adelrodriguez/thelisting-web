8beb484f4f98ab939c47639a14cd49e4521d7c7e
Adel Rodríguez <hello@adelrodriguez.com>
2023-04-18 18:18:20 -0400

    remove unused schemas

--- app/utils/listing.ts
+++ app/utils/listing.ts
@@ -2,13 +2,9 @@ import { ListingStatus, ListingType } from "@prisma/client"
 import { startOfToday } from "date-fns"
 import { z } from "zod"
 
-import db from "~/helpers/db.server"
-
 export const ListingEventDateSchema = z.coerce
   .date()
   .min(startOfToday(), { message: "Event date must be in the future" })
-
-export const EventDateSchema = ListingEventDateSchema // TODO(adelrodriguez): REMOVE THIS
 
 export const ListingPathSchema = z
   .string()
@@ -17,15 +13,9 @@ export const ListingPathSchema = z
     message: "Path can only contain letters, numbers and dashes",
   })
 
-export const PathSchema = ListingPathSchema // TODO(adelrodriguez): REMOVE THIS
-
 export const ListingTitleSchema = z.string().min(3, "Please enter a title")
-export const TitleSchema = ListingTitleSchema // TODO(adelrodriguez): REMOVE THIS
 
 export const ListingSubtitleSchema = z.string().optional()
-export const SubtitleSchema = z.string().nullish()
-
-export const CommerceIdSchema = z.string().nullish()
 
 export const ListingTypeSchema = z.enum(
   [
@@ -38,10 +28,6 @@ export const ListingTypeSchema = z.enum(
     errorMap: () => ({ message: "Please select a type of event" }),
   }
 )
-/**
- * @deprecated
- */
-export const TypeSchema = ListingTypeSchema // TODO: REMOVE THIS
 
 export const ListingOwnerSchema = z
   .string()
@@ -54,23 +40,7 @@ export const ListingStatusSchema = z.enum(
   }
 )
 
-/**
- * @deprecated
- */
-export const StatusSchema = ListingStatusSchema // TODO: REMOVE THIS
-
 export const ListingCoverImageSchema = z
   .string()
   .min(1, "Please provide an image")
 export const ListingThankYouImageSchema = z.string().optional()
-
-export const ImageSchema = z.string().nullish().optional()
-
-export async function verifyPathIsUnique(path: string) {
-  const listing = await db.listing.findUnique({
-    select: { id: true },
-    where: { path },
-  })
-
-  return !listing
-}
