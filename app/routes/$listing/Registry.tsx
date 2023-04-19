2a156d5cdb3ae50d1061480d1cc89e1176b7e8c3
Adel Rodríguez <hello@adelrodriguez.com>
2023-04-19 11:36:31 -0400

    add animation to registry button

--- app/routes/$listing/Registry.tsx
+++ app/routes/$listing/Registry.tsx
@@ -1,6 +1,8 @@
 import { ShoppingBagIcon } from "@heroicons/react/24/outline"
 import type { Item } from "@prisma/client"
 import { Link } from "@remix-run/react"
+import { useAnimate } from "framer-motion"
+import { useCallback, useEffect } from "react"
 
 import { useCart } from "~/utils/hooks"
 import { sortByQuantity } from "~/utils/item"
@@ -9,6 +11,22 @@ import RegistryItem from "./RegistryItem"
 
 export default function Registry({ items }: { items: Item[] }) {
   const cart = useCart()
+  const [scope, animate] = useAnimate()
+  const shake = useCallback(() => {
+    animate(
+      scope.current,
+      {
+        rotate: [0, -12, 12, -12, 12, 0],
+      },
+      { duration: 1.5, ease: "easeInOut" }
+    )
+  }, [animate, scope])
+
+  useEffect(() => {
+    if (cart.itemCount === 0) return
+
+    shake()
+  }, [cart.itemCount, shake])
 
   return (
     <div className="grid grid-cols-2 gap-y-8 gap-x-4 md:grid-cols-3 md:gap-x-8 xl:grid-cols-4 xl:gap-x-10">
@@ -26,19 +44,19 @@ export default function Registry({ items }: { items: Item[] }) {
           />
         )
       })}
-      <div className="z-2 group fixed bottom-8 right-8">
+      <div className="group fixed bottom-8 right-8 z-10" ref={scope}>
         <Link to="cart" relative="path" prefetch="intent" preventScrollReset>
           {!!cart.itemCount && (
-            <div className="absolute top-0 right-0 z-10 flex h-6 w-6 items-center justify-center rounded-full border-2 border-white bg-red-500 text-xs text-white ">
+            <div className="absolute top-0 right-0 z-10 flex h-6 w-6 items-center justify-center rounded-full border-2 border-white bg-red-500 text-xs text-white md:h-7 md:w-7 md:text-sm">
               {cart.itemCount}
             </div>
           )}
 
           <button
             title="Go to cart"
-            className="flex h-16 w-16 items-center justify-center rounded-full bg-gray-600 text-4xl text-white drop-shadow-xl duration-300 group-hover:shadow-2xl"
+            className="flex h-16 w-16 items-center justify-center rounded-full bg-slate-600 text-4xl text-white drop-shadow-xl duration-300 group-hover:shadow-2xl md:h-20 md:w-20"
           >
-            <ShoppingBagIcon className="h-8 w-8 duration-300 group-hover:h-9  group-hover:w-9" />
+            <ShoppingBagIcon className="h-8 w-8 duration-300 group-hover:h-9 group-hover:w-9 md:h-10 md:w-10 md:group-hover:h-12 md:group-hover:w-12" />
           </button>
         </Link>
       </div>
