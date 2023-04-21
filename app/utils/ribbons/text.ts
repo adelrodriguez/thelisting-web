48e3de79ed3366c0a2d4ba9215aa12e9c1df94b5
Adel Rodríguez <hello@adelrodriguez.com>
2023-04-21 13:44:55 -0400

    change utils to .ts

--- app/utils/ribbons/text.ts
+++ app/utils/ribbons/text.ts
@@ -6,8 +6,10 @@ import { RibbonBaseSchema } from "./base"
 export const TextPropertiesSchema = z.object({
   body: z.string().min(1, "You must provide a body for the text"),
   decorationImage: z.string().optional(),
+  label: z.string().optional(),
   textAlignment: z.string().optional(),
   title: z.string().optional(),
+  url: z.string().url().optional(),
 })
 export type TextProperties = z.infer<typeof TextPropertiesSchema>
 
