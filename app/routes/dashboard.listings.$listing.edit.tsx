903d58ca8b1118218545692de112cd7c192085a1
Adel Rodríguez <hello@adelrodriguez.com>
2023-04-25 14:41:50 -0400

    add edit page

---
+++ app/routes/dashboard.listings.$listing.edit.tsx
@@ -0,0 +1,361 @@
+import { ListingStatus, ListingType, UserRole } from "@prisma/client"
+import type { ActionArgs, LoaderArgs } from "@remix-run/node"
+import { json } from "@remix-run/node"
+import type { RouteMatch } from "@remix-run/react"
+import { useActionData, useLoaderData } from "@remix-run/react"
+import { withZod } from "@remix-validated-form/with-zod"
+import { format, subMilliseconds } from "date-fns"
+import { getTimezoneOffset } from "date-fns-tz"
+import { StatusCodes } from "http-status-codes"
+import { useSnackbar } from "notistack"
+import { useEffect } from "react"
+import { notFound } from "remix-utils"
+import { setFormDefaults } from "remix-validated-form"
+import { z } from "zod"
+import { zfd } from "zod-form-data"
+import { zx } from "zodix"
+
+import {
+  Checkbox,
+  Form,
+  ImageInput,
+  Input,
+  InputWithAddOn,
+  ListRadioGroup,
+  Select,
+  SubmitButton,
+} from "~/components/form"
+import auth from "~/helpers/auth.server"
+import {
+  ListingCoverImageSchema,
+  ListingEventDateSchema,
+  ListingOwnerSchema,
+  ListingPathSchema,
+  ListingStatusSchema,
+  ListingSubtitleSchema,
+  ListingThankYouImageSchema,
+  ListingTitleSchema,
+  ListingTypeSchema,
+} from "~/utils/listing"
+import { forbidden, unauthorized } from "~/utils/remix"
+import { getUserFullName } from "~/utils/user"
+
+export const handle = {
+  crumb: ({ params }: RouteMatch) => ({
+    href: `/dashboard/listings/${params.listing}/details/edit`,
+    name: "Edit Listing",
+  }),
+  id: "dashboard-listings-details",
+}
+
+const clientValidator = withZod(
+  z.object({
+    coverImage: ListingCoverImageSchema,
+    eventDate: ListingEventDateSchema,
+    isInternal: zfd.checkbox({ trueValue: "internal" }),
+    ownerId: ListingOwnerSchema,
+    path: ListingPathSchema,
+    status: ListingStatusSchema,
+    subtitle: z.string().optional(),
+    thankYouImage: ListingThankYouImageSchema,
+    timezone: z.string().optional(),
+    title: ListingTitleSchema,
+    type: ListingTypeSchema,
+  })
+)
+
+export async function loader({ params, context }: LoaderArgs) {
+  const db = context.db
+  const { listing: sku } = zx.parseParams(params, {
+    listing: z.coerce.number(),
+  })
+
+  const [listing, users] = await Promise.all([
+    db.listing.findUnique({ where: { sku } }),
+    db.user.findMany({
+      orderBy: { firstName: "asc" },
+      select: { firstName: true, id: true, lastName: true },
+    }),
+  ])
+
+  if (!listing) {
+    throw notFound({ message: "Listing not found.", title: "Not Found" })
+  }
+
+  return json({
+    users,
+    ...setFormDefaults("edit-listing", {
+      ...listing,
+      eventDate: format(listing.eventDate, "yyyy-MM-dd"),
+    }),
+  })
+}
+
+export async function action({ request, params, context }: ActionArgs) {
+  const db = context.db
+  const user = await auth.isAuthenticated(request)
+
+  if (!user) {
+    throw unauthorized({
+      message: "You must be logged in to do that.",
+      title: "Unauthorized",
+    })
+  }
+
+  const { listing: sku } = zx.parseParams(
+    params,
+    z.object({ listing: z.coerce.number() })
+  )
+
+  const listing = await db.listing.findUniqueOrThrow({
+    where: { sku: Number(sku) },
+  })
+
+  if (listing.ownerId !== user.id && user.role !== UserRole.Admin) {
+    throw forbidden({
+      message: "You do not have permission to update this listing.",
+      title: "Forbidden",
+    })
+  }
+
+  const formData = await request.formData()
+  const timezone = formData.get("timezone") as string
+
+  const serverValidator = withZod(
+    z.object({
+      coverImage: ListingCoverImageSchema,
+      eventDate: ListingEventDateSchema.transform((value) => {
+        const timezoneOffsetInMilliseconds = getTimezoneOffset(timezone, value)
+
+        return subMilliseconds(value, timezoneOffsetInMilliseconds)
+      }),
+      isInternal: zfd.checkbox({ trueValue: "internal" }),
+      ownerId: ListingOwnerSchema,
+      path: ListingPathSchema.trim()
+        .transform((value) => value.toLowerCase())
+        .refine(
+          async (path) => {
+            const existingListing = await db.listing.findFirst({
+              select: { id: true },
+              where: { path },
+            })
+
+            if (!existingListing) return true
+
+            return existingListing.id === listing.id
+          },
+          { message: "The path you have entered is already in use." }
+        ),
+      status: ListingStatusSchema,
+      subtitle: ListingSubtitleSchema,
+      thankYouImage: ListingThankYouImageSchema,
+      title: ListingTitleSchema,
+      type: ListingTypeSchema,
+    })
+  )
+
+  const result = await serverValidator.validate(formData)
+
+  if (result.error) {
+    return json(
+      { ...result.error, listing: null, success: false },
+      { status: StatusCodes.UNPROCESSABLE_ENTITY }
+    )
+  }
+
+  const updatedListing = await db.listing.update({
+    data: result.data,
+    where: { id: listing.id },
+  })
+
+  if (updatedListing.status !== listing.status) {
+    switch (updatedListing.status) {
+      case ListingStatus.Published:
+        await db.listing.update({
+          data: {
+            closedAt: null,
+            publishedAt: new Date(),
+          },
+          where: { id: updatedListing.id },
+        })
+        break
+      case ListingStatus.Closed:
+        await db.listing.update({
+          data: {
+            closedAt: new Date(),
+          },
+          where: { id: updatedListing.id },
+        })
+        break
+      default:
+        await db.listing.update({
+          data: {
+            closedAt: null,
+            publishedAt: null,
+          },
+          where: { id: updatedListing.id },
+        })
+        break
+    }
+  }
+
+  return json({ listing: updatedListing, success: true })
+}
+
+export default function DashboardListingPage() {
+  const { enqueueSnackbar } = useSnackbar()
+  const { users } = useLoaderData<typeof loader>()
+  const actionData = useActionData<typeof action>()
+
+  useEffect(() => {
+    if (!actionData) return
+
+    if (actionData.success) {
+      enqueueSnackbar("Listing updated 🎉", {
+        description: "The listing was successfully updated",
+        variant: "success",
+      })
+    } else {
+      enqueueSnackbar("Error updating listing", {
+        description: "There was an error updating the listing",
+        variant: "error",
+      })
+    }
+  }, [actionData, enqueueSnackbar])
+
+  return (
+    <Form
+      id="edit-listing"
+      className="m-auto mt-8 flex w-full max-w-xl flex-col gap-y-6"
+      method="POST"
+      validator={clientValidator}
+    >
+      <Input
+        label="Title"
+        name="title"
+        description="This is what we'll call your listing and show to others"
+        required
+      />
+      <Input
+        label="Subtitle"
+        name="subtitle"
+        description="This will appear under the title and in the description when sharing your listing"
+      />
+      <Input
+        label="SKU"
+        name="sku"
+        disabled
+        description="This is the unique identifier for your listing"
+      />
+      <InputWithAddOn
+        label="Path"
+        name="path"
+        description="This is the URL for your listing"
+        addOn={"https://giftthelisting.com/"}
+        required
+      />
+      <Input
+        description="The date of your event"
+        label="Event Date"
+        name="eventDate"
+        required
+        type="date"
+      />
+      <input
+        type="hidden"
+        name="timezone"
+        value={Intl.DateTimeFormat().resolvedOptions().timeZone}
+      />
+      <Select
+        options={[
+          {
+            label: "Select an option",
+            value: "",
+          },
+          {
+            label: "💍 Wedding",
+            value: ListingType.Wedding,
+          },
+          {
+            label: "🍼 Baby Shower",
+            value: ListingType.BabyShower,
+          },
+          {
+            label: "🎂 Birthday",
+            value: ListingType.Birthday,
+          },
+          {
+            label: "❓ Other",
+            value: ListingType.Other,
+          },
+        ]}
+        label="Event Type"
+        name="type"
+        description="The type of event you're hosting"
+        required
+      />
+      <Select
+        options={[
+          { label: "Select an option", value: "" },
+          ...users.map((user) => ({
+            label: getUserFullName(user),
+            value: user.id,
+          })),
+        ]}
+        label="Owner"
+        name="ownerId"
+        description="The user owner of this listing"
+        required
+      />
+      <ListRadioGroup
+        name="status"
+        label="Status"
+        options={[
+          {
+            description:
+              "The listing is not visible to others, you can still edit it and add items",
+            label: "Draft",
+            value: ListingStatus.Draft,
+          },
+          {
+            description: "The listing is visible to others",
+            label: "Published",
+            value: ListingStatus.Published,
+          },
+          {
+            description:
+              "The listing is closed, it is no longer visible to others and you can no longer edit it",
+            label: "Closed",
+            value: ListingStatus.Closed,
+          },
+        ]}
+        required
+      />
+      <ImageInput
+        label="Registry Cover Image"
+        name="coverImage"
+        description="The image that will be shown on the registry page"
+        placeholder="cover.png"
+      />
+      <ImageInput
+        label={'"Thank You" Image'}
+        name="thankYouImage"
+        description="The image that will be shown after someone purchases an item from your listing"
+        placeholder="thank-you.png"
+      />
+      <Input
+        description="The Shopify collection ID. You need to have this in order to be able to add items to your listing. If this is empty and you recently created the listing, please wait a few seconds."
+        label="Commerce ID"
+        name="commerceId"
+        disabled
+      />
+      <Checkbox
+        label="Internal"
+        name="isInternal"
+        description="Internal listings are used for showcase purposes. Purchases are disabled."
+        value="internal"
+      />
+      <SubmitButton loadingText="Updating...">Update</SubmitButton>
+    </Form>
+  )
+}
