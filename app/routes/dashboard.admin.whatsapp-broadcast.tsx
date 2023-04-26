d151f8300558fbe34c616be0ab532da145ce0d1e
Adel Rodríguez <hello@adelrodriguez.com>
2023-04-26 16:09:10 -0400

    improve whatsapp broadcast form

--- app/routes/dashboard.admin.whatsapp-broadcast.tsx
+++ app/routes/dashboard.admin.whatsapp-broadcast.tsx
@@ -1,15 +1,28 @@
+import { MinusIcon } from "@heroicons/react/20/solid"
 import type { ActionArgs } from "@remix-run/node"
 import { json } from "@remix-run/node"
 import { useActionData } from "@remix-run/react"
 import { withZod } from "@remix-validated-form/with-zod"
 import { useSnackbar } from "notistack"
 import { useEffect } from "react"
-import { ValidatedForm, validationError } from "remix-validated-form"
+import {
+  useFieldArray,
+  ValidatedForm,
+  validationError,
+} from "remix-validated-form"
 import { z } from "zod"
 
-import { FormInput, FormSelect, FormSubmit, TextArea } from "~/components/form"
+import { Button } from "~/components/common"
+import {
+  ImageInput,
+  Input,
+  InputWithAddOn,
+  Select,
+  SubmitButton,
+} from "~/components/form"
 import { WHATSAPP_MESSAGE_TEMPLATES } from "~/config/consts"
 import whatsapp from "~/services/whatsapp.server"
+import { generateCloudflareImageUrl } from "~/utils/cloudflare"
 
 export const handle = {
   crumb: () => ({
@@ -18,28 +31,26 @@ export const handle = {
   }),
 }
 
-const whatsAppBroadcastFormSchema = z.object({
-  customer: z.string().min(1),
-  mediaUrl: z.string().url(),
-  path: z.string().min(1),
-  phoneNumbers: z
-    .string()
-    .min(1)
-    .refine((value) => value.split(",").length > 0, {
-      message: "Please enter at least one phone number",
-    }),
-  template: z.enum(
-    [
-      WHATSAPP_MESSAGE_TEMPLATES.BabyShowerGuestNotification,
-      WHATSAPP_MESSAGE_TEMPLATES.WeddingGuestNotification,
-    ],
-    {
-      errorMap: () => ({ message: "Please select a template" }),
-    }
-  ),
-})
+const validator = withZod(
+  z.object({
+    customer: z.string().min(1),
+    image: z.string().uuid("You must provide an image"),
+    path: z.string().min(1),
+    phoneNumbers: z
+      .array(z.string())
+      .min(1, { message: "Please add a number" }),
 
-const validator = withZod(whatsAppBroadcastFormSchema)
+    template: z.enum(
+      [
+        WHATSAPP_MESSAGE_TEMPLATES.BabyShowerGuestNotification,
+        WHATSAPP_MESSAGE_TEMPLATES.WeddingGuestNotification,
+      ],
+      {
+        errorMap: () => ({ message: "Please select a template" }),
+      }
+    ),
+  })
+)
 
 export async function action({ request }: ActionArgs) {
   const formData = await request.formData()
@@ -48,7 +59,7 @@ export async function action({ request }: ActionArgs) {
   if (error) return validationError(error)
 
   const response = await Promise.allSettled(
-    data.phoneNumbers.split(",").map(async (phoneNumber) => {
+    data.phoneNumbers.map(async (phoneNumber) => {
       try {
         // We are awaiting the promise here because we want to catch any errors
         return await whatsapp.sendGuestNotification(
@@ -56,7 +67,7 @@ export async function action({ request }: ActionArgs) {
           phoneNumber,
           {
             customer: data.customer,
-            mediaUrl: data.mediaUrl,
+            mediaUrl: generateCloudflareImageUrl(data.image, "public"),
             path: data.path,
           }
         )
@@ -74,6 +85,9 @@ export async function action({ request }: ActionArgs) {
 export default function WhatsAppBroadcastPage() {
   const data = useActionData<typeof action>()
   const { enqueueSnackbar } = useSnackbar()
+  const [inputs, { push, remove }] = useFieldArray("phoneNumbers", {
+    formId: "whatsapp-broadcast",
+  })
 
   useEffect(() => {
     if (!data) return
@@ -112,8 +126,9 @@ export default function WhatsAppBroadcastPage() {
         validator={validator}
         className="m-auto mt-8 flex flex-col gap-y-6 sm:w-[500px]"
         method="POST"
+        id="whatsapp-broadcast"
       >
-        <FormSelect
+        <Select
           label="Template"
           name="template"
           options={[
@@ -133,24 +148,59 @@ export default function WhatsAppBroadcastPage() {
           placeholder="Select a template"
           required
         />
-        <FormInput name="path" label="Path" addOn="https://thelisting.do/" />
-        <FormInput
+        <InputWithAddOn
+          name="path"
+          label="Path"
+          addOn="https://thelisting.do/"
+        />
+        <Input
           name="customer"
           label="Customer"
           description="The name of the customer(s) (e.g. José y María)"
         />
-        <FormInput
-          name="mediaUrl"
-          label="Media URL"
-          description="The URL to the media to attach to the message"
+        <ImageInput
+          name="image"
+          label="Message Image"
+          description="The image to attach to the message"
         />
-        <TextArea
-          name="phoneNumbers"
-          label="Phone Numbers"
-          description="Comma-separated list of phone numbers with country codes (e.g. 18091234567,18097654321)"
-          rows={4}
-        />
-        <FormSubmit />
+        <div className="flex items-start justify-between">
+          <div className="flex-1">
+            <h3 className="font-semibold">Phone Numbers</h3>
+            <p className="text-xs">
+              The phone number to send the message to, including the country
+              code (e.g. +18091234567)
+            </p>
+          </div>
+
+          <Button
+            onClick={() => push("")}
+            type="button"
+            size="xs"
+            variant="secondary"
+          >
+            Add a number
+          </Button>
+        </div>
+        <div className="flex flex-col gap-y-2">
+          {inputs.map((_, index) => (
+            <div key={`inputs${index}`} className="flex w-full items-end">
+              <Input
+                name={`phoneNumbers[${index}]`}
+                label={`Phone Number ${index + 1}`}
+                className="mr-2 w-full"
+              />
+              <Button
+                onClick={() => remove(index)}
+                className="my-1"
+                variant="secondary"
+                type="button"
+              >
+                <MinusIcon className="h-5 w-5" />
+              </Button>
+            </div>
+          ))}
+        </div>
+        <SubmitButton loadingText="Sending...">Send</SubmitButton>
       </ValidatedForm>
     </div>
   )
