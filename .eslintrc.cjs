2910cab860a608292fe5f85c3df7678768d21010
Adel Rodríguez <hello@adelrodriguez.com>
2023-05-22 16:01:35 -0400

    update linter config

---
+++ .eslintrc.cjs
@@ -0,0 +1,42 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "prettier",
+  ],
+
+  overrides: [
+    {
+      extends: ["plugin:@typescript-eslint/recommended"],
+      files: ["./**/*.{ts,tsx}"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: __dirname,
+      },
+      plugins: ["@typescript-eslint"],
+      rules: {
+        "@typescript-eslint/no-floating-promises": "error",
+        "@typescript-eslint/no-unused-vars": "warn",
+      },
+    },
+  ],
+  plugins: ["sort-keys"],
+  root: true,
+  rules: {
+    "no-console": "warn",
+    "no-unused-vars": "off",
+    "sort-keys/sort-keys-fix": "warn",
+    "spaced-comment": [
+      "error",
+      "always",
+      {
+        line: {
+          markers: ["/"],
+        },
+      },
+    ],
+  },
+}
