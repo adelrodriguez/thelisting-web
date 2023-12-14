8046472d4e9ed123978c88a6c5c5b81a0915059e
Adel Rodríguez <hello@adelrodriguez.com>
2023-12-14 09:34:51 -0400

    refactor: remove comments

--- app/routes/_auth.magic/route.tsx
+++ app/routes/_auth.magic/route.tsx
@@ -5,14 +5,7 @@ import auth from "~/helpers/auth.server"
 
 export async function loader({ request }: LoaderFunctionArgs) {
   await auth.authenticate("email-link", request, {
-    // If something failed we take them back to the login page
-    // This redirect is optional, if not defined any error will be throw and
-    // the ErrorBoundary will be rendered.
     failureRedirect: route("/register"),
-    // If the user was authenticated, we redirect them to their profile page
-    // This redirect is optional, if not defined the user will be returned by
-    // the `authenticate` function and you can render something on this page
-    // manually redirect the user.
     successRedirect: route("/dashboard"),
   })
 }
