7dee412fddf2f7df264a144f1533468809613d5a
Adel Rodríguez <hello@adelrodriguez.com>
2023-12-14 10:38:54 -0400

    fix: update mocks

--- mocks/services/alegra.ts
+++ mocks/services/alegra.ts
@@ -1,10 +1,5 @@
 import { generateMock } from "@anatine/zod-mock"
-import type {
-  DefaultRequestMultipartBody,
-  MockedRequest,
-  RestHandler,
-} from "msw"
-import { rest } from "msw"
+import { HttpResponse, http } from "msw"
 
 import {
   CreateContactResponseSchema,
@@ -14,41 +9,30 @@ import {
   SendInvoiceResponseSchema,
 } from "~/utils/alegra"
 
-export const alegraHandlers: Array<
-  RestHandler<MockedRequest<DefaultRequestMultipartBody>>
-> = [
-  rest.post("https://app.alegra.com/api/v1/contacts", async (req, res, ctx) => {
+export const alegraHandlers = [
+  http.post("https://app.alegra.com/api/v1/contacts", async () => {
     const response = generateMock(CreateContactResponseSchema)
 
-    return res(ctx.json(response))
+    return HttpResponse.json(response)
   }),
-  rest.get(
-    "https://app.alegra.com/api/v1/contacts/:id",
-    async (req, res, ctx) => {
-      const response = generateMock(GetContactResponseSchema)
+  http.get("https://app.alegra.com/api/v1/contacts/:id", async () => {
+    const response = generateMock(GetContactResponseSchema)
 
-      return res(ctx.json(response))
-    },
-  ),
-  rest.get(
-    "https://app.alegra.com/api/v1/currencies/:code",
-    async (req, res, ctx) => {
-      const response = generateMock(GetCurrencyResponseSchema)
+    return HttpResponse.json(response)
+  }),
+  http.get("https://app.alegra.com/api/v1/currencies/:code", async () => {
+    const response = generateMock(GetCurrencyResponseSchema)
 
-      return res(ctx.json(response))
-    },
-  ),
-  rest.post("https://app.alegra.com/api/v1/invoices", async (req, res, ctx) => {
+    return HttpResponse.json(response)
+  }),
+  http.post("https://app.alegra.com/api/v1/invoices", async () => {
     const response = generateMock(CreateInvoiceResponseSchema)
 
-    return res(ctx.json(response))
+    return HttpResponse.json(response)
   }),
-  rest.post(
-    "https://app.alegra.com/api/v1/invoices/:id/email",
-    async (req, res, ctx) => {
-      const response = generateMock(SendInvoiceResponseSchema)
+  http.post("https://app.alegra.com/api/v1/invoices/:id/email", async () => {
+    const response = generateMock(SendInvoiceResponseSchema)
 
-      return res(ctx.json(response))
-    },
-  ),
+    return HttpResponse.json(response)
+  }),
 ]
