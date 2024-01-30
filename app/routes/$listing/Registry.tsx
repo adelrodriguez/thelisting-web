0a7b83c19f067a65b2b14c943c8f2eb34e1c606a
Adel Rodríguez <hello@adelrodriguez.com>
2024-01-30 17:27:02 -0400

    fix: keep query params on Registry

--- app/routes/$listing/Registry.tsx
+++ app/routes/$listing/Registry.tsx
@@ -1,5 +1,5 @@
 import { ShoppingBagIcon } from "@heroicons/react/24/outline"
-import { Link } from "@remix-run/react"
+import { Link, useLocation } from "@remix-run/react"
 import { useAnimate } from "framer-motion"
 import { ReactNode, useCallback, useEffect } from "react"
 
@@ -7,6 +7,7 @@ import { calculateItemCount } from "~/utils/cart"
 import { useCart } from "~/utils/hooks"
 
 export default function Registry({ children }: { children: ReactNode }) {
+  const location = useLocation()
   const cart = useCart()
   const [scope, animate] = useAnimate()
   const shake = useCallback(() => {
@@ -31,7 +32,12 @@ export default function Registry({ children }: { children: ReactNode }) {
     <div className="grid grid-cols-2 gap-x-4 gap-y-8 md:grid-cols-3 md:gap-x-8 xl:grid-cols-4 xl:gap-x-10">
       {children}
       <div className="group fixed bottom-8 right-8 z-10" ref={scope}>
-        <Link prefetch="intent" preventScrollReset relative="path" to="cart">
+        <Link
+          prefetch="intent"
+          preventScrollReset
+          relative="path"
+          to={"cart" + location.search}
+        >
           {!!itemCount && (
             <div className="absolute right-0 top-0 z-10 flex h-6 w-6 items-center justify-center rounded-full border-2 border-white bg-red-500 text-xs text-white md:h-7 md:w-7 md:text-sm">
               {itemCount}
